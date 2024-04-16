1c1b15f79dbb9da0271e46b519dab50b5ea787f9
Tancred1014 <tancred.shih@gmail.com.tw>
2024-04-16 17:37:43 +0800

    add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,4 +1,8 @@
 const dayjs = require('dayjs') // 載入 dayjs 套件
 module.exports = {
-  currentYear: () => dayjs().year() // 取得當年年份作為 currentYear 的屬性值，並導出
+  currentYear: () => dayjs().year(), // 取得當年年份作為 currentYear 的屬性值，並導出
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
+  // 傳入的參數 a 和 b 對應到文件上的 conditions。return 的部分，我們用了三元運算子來簡化 if/else 流程，若 a 和 b 相等，會回傳 options.fn(this)，不相等則回傳 options.inverse(this)，和官方文件中的流程是一樣的。
 }
