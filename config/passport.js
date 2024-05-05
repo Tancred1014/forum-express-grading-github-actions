b88a2742962a41922c460a23f747af88747733c7
Tancred1014 <tancred.shih@gmail.com.tw>
2024-05-05 14:00:51 +0800

    get user's favorited restaurants & switch button

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const User = db.User
+const { User, Restaurant } = require('../models')
 // set up Passport strategy
 passport.use(new LocalStrategy(
   // customize user field
@@ -28,10 +27,14 @@ passport.serializeUser((user, cb) => {
   cb(null, user.id)
 })
 passport.deserializeUser((id, cb) => {
-  User.findByPk(id).then(user => {
-    user = user.toJSON()
-
-    return cb(null, user)
+  return User.findByPk(id, {
+    include: [
+      { model: Restaurant, as: 'FavoritedRestaurants' }
+      // 注意在 include 的時候有追加 as: 'FavoritedRestaurants'
+      // 這時候我們用 as 來標明我們想要引入的關係，而這個 as 會對應到我們剛剛在 model 裡設定的別名
+    ]
   })
+    .then(user => cb(null, user.toJSON()))
+    .catch(err => cb(err))
 })
 module.exports = passport
