1666844867c2fae0805d1dbe8448133daf1bb75b
Tancred1014 <tancred.shih@gmail.com.tw>
2024-05-05 13:20:22 +0800

    add favorite/unfavorite button at index page

--- routes/index.js
+++ routes/index.js
@@ -31,6 +31,8 @@ router.post('/comments', authenticated, commentController.postComment)
 // 如果一個請求是走 GET /restaurants 進來，會需要先經過 auth.js 這支 middleware 進行身分驗證，通過才能呼叫 restController.getRestaurants 來顯示餐廳清單的主頁
 // 如果接收到的請求路徑是 / restaurants，那就交給 controller 的getRestaurants
 //  函式來處理。如果這行路由和請求匹配成功，以下的 router.get 就不會執行。
+router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 router.use('/', (req, res) => res.redirect('/restaurants'))// 設定 fallback 路由
 router.use('/', generalErrorHandler)
 
