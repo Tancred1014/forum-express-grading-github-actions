13be12d87c300e52adc0c136904af9bdb627f799
Tancred1014 <tancred.shih@gmail.com.tw>
2024-05-10 15:16:38 +0800

    add top-restaurants page and router

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,7 @@ router.delete('/following/:userId', authenticated, userController.removeFollowin
 
 // restaurants
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
+router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
