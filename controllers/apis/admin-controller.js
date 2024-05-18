12af88a5c590cdbc157875ed0abbe3f186e273a3
Tancred1014 <tancred.shih@gmail.com.tw>
2024-05-18 12:04:57 +0800

    add create restaurant api

--- controllers/apis/admin-controller.js
+++ controllers/apis/admin-controller.js
@@ -1,7 +1,12 @@
 const adminServices = require('../../services/admin-services')
+// const { postRestaurant } = require('../pages/admin-controller')
+
 const adminController = {
   getRestaurants: (req, res, next) => {
     adminServices.getRestaurants(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  },
+  postRestaurant: (req, res, next) => {
+    adminServices.postRestaurant(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   deleteRestaurant: (req, res, next) => {
     adminServices.deleteRestaurant(req, (err, data) => err ? next(err) : res.json(data))
--- services/admin-services.js
+++ services/admin-services.js
@@ -1,4 +1,6 @@
 const { Restaurant, Category } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+
 const adminServices = {
   getRestaurants: (req, cb) => {
     Restaurant.findAll({
@@ -7,6 +9,23 @@ const adminServices = {
       include: [Category] // 當你想要使用 model 的關聯資料時，需要透過 include 把關聯資料拉進來，關聯資料才會被拿到 findAll 的回傳值裡。
     })
       .then(restaurants => cb(null, { restaurants }))
+      .catch(err => cb(err))
+  },
+  postRestaurant: (req, cb) => {
+    const { name, tel, address, openingHours, description, categoryId } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    const { file } = req // 把檔案取出來，也可以寫成 const file = req.file
+    imgurFileHandler(file) // 把取出的檔案傳給 file-helper 處理後
+      .then(filePath => Restaurant.create({ // 再 create 這筆餐廳資料
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image: filePath || null,
+        categoryId
+      }))
+      .then(newRestaurant => cb(null, { restaurant: newRestaurant }))
       .catch(err => cb(err))
   },
   deleteRestaurant: (req, cb) => {
