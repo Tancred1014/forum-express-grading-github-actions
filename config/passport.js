e0d18ad01dd708c35d78ae77c1133be44352221b
Tancred1014 <tancred.shih@gmail.com.tw>
2024-05-20 15:32:07 +0800

    add api-auth middleware

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,11 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const passportJWT = require('passport-jwt')
 const bcrypt = require('bcryptjs')
 const { User, Restaurant } = require('../models')
+const JETStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+
 // set up Passport strategy
 passport.use(new LocalStrategy(
   // customize user field
@@ -17,11 +21,28 @@ passport.use(new LocalStrategy(
         if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
         bcrypt.compare(password, user.password).then(res => {
           if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+
           return cb(null, user)
         })
       })
   }
 ))
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+}
+passport.use(new JETStrategy(jwtOptions, (jwtPayload, cb) => {
+  User.findByPk(jwtPayload.id, {
+    include: [
+      { model: Restaurant, as: 'FavoritedRestaurants' },
+      { model: Restaurant, as: 'LikedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  })
+}))
+
 // serialize and deserialize user
 passport.serializeUser((user, cb) => {
   cb(null, user.id)
